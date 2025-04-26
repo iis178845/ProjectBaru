8f213968d431302feda386994bc54b83a2ca73c3
iis178845 <ottoarkintas@gmail.com>
2025-04-26 20:09:16 +0700

    up

--- App.js
+++ App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Teks dengan styling dasar */}
+      <Text style={styles.title}>Selamat Datang di React Native!</Text>
+
+      {/*Teks dengan warna dan ukuran berbeda */}
+      <Text style={styles.subtitle}>Belajar Text Componet dan Styling</Text>
+
+      {/*Teks dengan gaya italic dan bold */}
+      <Text style={styles.emphasis}>Ini adalah teks penting</Text>
+
+      {/*Contoh teks multiline dengan batasan baris */}
+      <Text numberOfLines={3} ellipsizeMode="tail" style={styles.multiline}>
+        Ini adalah contoh teks yang sangat panjang sehingga akan dipotong jika melebihi dua baris.
+        Fitur ini berguna untuk menjaga tampilan tetap rapi.
+      </Text>
+
     </View>
   );
 }
@@ -14,7 +27,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center',       // Tengah horizontal
+    justifyContent: 'center',   // Tengah vertikal
+    paddingHorizontal: 20,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333'
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+    marginBottom: 15,
+  },
+  emphasis: {
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    color: '#d9534f',
+    marginBottom: 15,
+  }, 
+  multiline:{
+    fontSize: 16,
+    color:'#444'
+  }
 });
