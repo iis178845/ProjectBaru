d68ba854fbbbefc51a950d0e3fd9b71b8d439d26
iis178845 <ottoarkintas@gmail.com>
2025-05-05 19:22:25 +0700

    Basic Styling React Native Component

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const StylingReactNativeComponent = () => {
           <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212, borderRadius: 8}} >
             <Image source={macbook} style={{width: 188, height: 107, borderRadius: 8}} />
             <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}} >New Macbook Pro 2019</Text>
-            <Text style={{fontSize: 12, fontWeight: 'bold', color: '#f2994A', marginTop: 16}} >Rp. @5.000.000</Text>
+            <Text style={{fontSize: 12, fontWeight: 'bold', color: '#f2994A', marginTop: 16}} >Rp. 25.000.000</Text>
             <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}} >Jakarta Barat</Text>
           <View style={{backgroundColor: '#6FCF97', paddingVertical: 6, borderRadius: 25, marginTop: 20}} >
             <Text style={{fontSize: 14, fontWeight: '600', color: 'white', textAlign: 'center'}} >BELI</Text>
